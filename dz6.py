9daf2467e34ae156ca060732c338188e562edfaa
rumia <rumia.azieva2020@gmail.com>
2026-01-30 18:15:35 +0500

    Домашнее задание 7

--- dz6.py
+++ dz6.py
@@ -1,6 +1,27 @@
 exit = False
 total_expenses: list[int] = []
-day_expense: int = 0
+value: int = 0
+
+
+def add_expense(total_expenses, value: int) -> list:  # Добавляет расход список и их значения
+    total_expenses.append(value)  # [100, 50, 50]
+    return total_expenses
+
+
+def delete_expense(total_expenses, index):  # Удаляет расход
+    total_expenses.pop(index-1)
+    return total_expenses
+
+
+def get_total(total_expenses):  # Вовзращает сумму
+    for ind, exp in enumerate(total_expenses):  # [1/100, 2/50, 50]
+        print(f"{ind+1}.{exp}руб.")
+    return total_expenses
+
+
+def get_average(total_expenses):  # Возвращает средний расход
+    average_expenses = sum(total_expenses) / len(total_expenses)
+    return average_expenses
 
 
 while exit is not True:
@@ -10,22 +31,24 @@ while exit is not True:
     print("4. Удалить расход по номеру")
     print("5. Выход")
     option = input("Выберите опцию №: ")
+
     if option == "1":
-        day_expense = int(input("Введите ваши расходы: "))
-        total_expenses.append(day_expense)  # [100, 50, 50]
+        value = int(input("Введите сумму вашего расхода: "))
+        total_expenses = add_expense(total_expenses, value)
 
     elif option == "2":
         print("Все ваши расходы:")
-        for ind, exp in enumerate(total_expenses):  # [1/100, 2/50, 50]
-            print(f"{ind+1}.{exp}руб.")
+        print(get_total(total_expenses))
 
     elif option == "3":
-        average_expenses = sum(total_expenses) / len(total_expenses)
+        average_expenses = get_average(total_expenses)
         print(f"Ваши расходы: {sum(total_expenses)} руб.")
         print(f"Ваши средние расходы: {int(average_expenses)} руб.")
+
     elif option == "4":
-        num_delete = int(input("Введите номер расходов для удаления: "))
-        total_expenses.pop(num_delete-1)
+        index = int(input("Введите номер расходов для удаления: "))
+        total_expenses = delete_expense(total_expenses, index)
+
     elif option == "5":
         exit = True
     else:
